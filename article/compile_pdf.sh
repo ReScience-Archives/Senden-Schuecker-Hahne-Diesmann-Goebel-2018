0e1c697083be7f0df207c5e20850137d2f059efe
Tiziano Zito <opossumnano@gmail.com>
2018-05-04 13:47:50 +0200

    --latex-engine option in pandoc has been removed

--- article/compile_pdf.sh
+++ article/compile_pdf.sh
@@ -1,4 +1,4 @@
-pandoc --standalone --filter pandoc-fignos --filter pandoc-tablenos --filter pandoc-eqnos --template=rescience-template.tex --latex-engine=xelatex --biblatex --bibliography=senden-schuecker-hahne-diesmann-goebel-2018.bib -M "crossrefYaml=crossref.yaml" --output senden-schuecker-hahne-diesmann-goebel-2018.tex senden-schuecker-hahne-diesmann-goebel-2018.md
+pandoc --standalone --filter pandoc-fignos --filter pandoc-tablenos --filter pandoc-eqnos --template=rescience-template.tex --pdf-engine=xelatex --biblatex --bibliography=senden-schuecker-hahne-diesmann-goebel-2018.bib -M "crossrefYaml=crossref.yaml" --output senden-schuecker-hahne-diesmann-goebel-2018.tex senden-schuecker-hahne-diesmann-goebel-2018.md
 
 xelatex senden-schuecker-hahne-diesmann-goebel-2018
 biber senden-schuecker-hahne-diesmann-goebel-2018
